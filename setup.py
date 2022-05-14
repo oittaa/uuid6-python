f74eda55833b91f91bcaf17b5529fd8b4e853771
oittaa <8972248+oittaa@users.noreply.github.com>
2022-05-14 17:09:03 +0300

    Add py.typed (#41)

--- setup.py
+++ setup.py
@@ -52,6 +52,9 @@ setup(
         "wheel",
     ],
     package_dir={"": "src"},
+    package_data={
+        "": ["py.typed"],
+    },
     packages=find_packages(where="src"),
     python_requires=">=3.7",
 )
