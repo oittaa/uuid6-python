350eccff3bd2f0dbc9e6667eac1c270adb15953a
oittaa <8972248+oittaa@users.noreply.github.com>
2024-11-18 05:10:16 +0100

    Add Python 3.13, drop Python 3.8 (#190)

--- setup.py
+++ setup.py
@@ -45,11 +45,11 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     setup_requires=[
         "wheel",
@@ -59,5 +59,5 @@ setup(
         "": ["py.typed"],
     },
     packages=find_packages(where="src"),
-    python_requires=">=3.8",
+    python_requires=">=3.9",
 )
